00a98d23a3218f9b782cee780a32c87a525cdd3f
Ivan Agapitov <50981092+IAgavan@users.noreply.github.com>
2019-11-23 14:53:44 +0300

    next gen2

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,39 @@
 $(document).ready(function () {
   $('.gallery').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots:true
+    dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  respondTo: "slider",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        
+      }
+    },
+    {
+      breakpoint: 620,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots:false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots:false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 });
