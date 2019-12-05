49edb4220c6c0a85cfad116718703c6128cedcf2
Ivan Agapitov <50981092+IAgavan@users.noreply.github.com>
2019-12-05 16:47:35 +0300

    another version

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,9 @@
-$(document).ready(function () {
-  $('.gallery').slick({
-    
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  speed: 1500,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  respondTo: "slider",
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3
-        
-      }
-    },
-    {
-      breakpoint: 620,
-      settings: {
-        slidesToShow: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-  });
-});
+const slides = document.querySelectorAll('.slider .slide');
+const slideInterval = setInterval(nextSlide,3000);
+let currentSlide = 0; 
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide displayed';
+}
