7ef3078ec4dd0f9645708fbee47f5c82f51627a6
Ivan Agapitov <50981092+IAgavan@users.noreply.github.com>
2019-12-12 12:54:58 +0300

    bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 // modal
 
 const modal = document.getElementById('modal');
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('.myBtn');
 const span = document.getElementsByClassName('close')[0];
 
-for (let i = 0; i<btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
         modal.style.display = "flex";
     }
@@ -13,11 +13,16 @@ span.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event){
-    if (event.target == modal){
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none"
     }
 }
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 27) {
+        modal.style.display = "none"
+    }
+});
 
 // slider_top
 const slides = document.querySelectorAll('.slider_top .slide_top');
@@ -37,3 +42,24 @@ function nextslide() {
     $('.swipebox').swipebox();
 
 })(jQuery);
+
+// <!-- кнопка вверх -->
+
+window.addEventListener('scroll', function () {
+    go_up.hidden = (pageYOffset < document.documentElement.clientHeight / 3);
+});
+
+
+
+// <!-- плавная прокрутка -->
+
+$(function () {
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
