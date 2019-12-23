a78c8ddf72d418b9137659deb0c6a454eaf7395d
Ivan Agapitov <50981092+IAgavan@users.noreply.github.com>
2019-12-23 12:44:19 +0300

    sidebar+

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,30 @@
+// Navbar toggler
 let menuElem = document.getElementById('navigation');
 let titleElem = document.querySelector('.menu-toggler');
 
 titleElem.onclick = function () {
   menuElem.classList.toggle('open');
+}
+
+
+
+
+// Short Recipes filter
+
+const inp = document.getElementsByClassName('inp1');
+let shortRecipeList = document.getElementsByClassName('short_recipe');
+
+function func(elm) {
+  for (let i = 0; i < shortRecipeList.length; i++) {
+    if (shortRecipeList[i].classList.contains(elm.id) && elm.checked) {
+      shortRecipeList[i].style.display = "flex";
+    } else
+      shortRecipeList[i].style.display = "none";
+  }
+}
+func(inp[0]);
+for (var i = 0; i < inp.length; i++) {
+  inp[i].onchange = function () {
+    func(this)
+  };
 }
